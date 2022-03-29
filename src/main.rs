3c6c7d4dac945ae320f82ddefdf20bf6f4c8dc5b
gaoxiangkang <gxk_717@163.com>
2022-03-29 17:09:34 +0800

    操作系统区分文件路径拼接符

--- src/main.rs
+++ src/main.rs
@@ -13,4 +13,5 @@ async fn main() {
     log4rs::init_file(path, Default::default()).unwrap();
     log::info!("开始加载项目, 时间为:[{}]...", util::get_date_time());
     methods::init().await.expect("方法初始化时异常！");
+
 }
